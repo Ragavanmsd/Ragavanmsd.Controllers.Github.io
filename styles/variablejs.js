edcb2faf2533bcfae6aa220b2d9a3fd08aa39c79
Ragavanmsd <75945143+Ragavanmsd@users.noreply.github.com>
2021-01-31 15:54:21 +0530

    Initial commit

---
+++ styles/variablejs.js
@@ -0,0 +1,39 @@
+const inputs=document.querySelectorAll('.controlers input');
+
+function handleChange(){
+    const suffix= this.dataset.sizing || '';
+    document.documentElement.style.setProperty(`--${this.name}` , this.value + suffix);
+    }
+
+inputs.forEach(input => input.addEventListener('change' , handleChange));
+inputs.forEach(input => input.addEventListener('mousemove' , handleChange))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
